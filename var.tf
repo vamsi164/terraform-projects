7b7433ce80a2d20afca2f639e71f554fa86d3117
root <root@ip-172-31-20-119.ec2.internal>
2024-11-17 16:00:01 +0000

    var.tf

---
+++ var.tf
@@ -0,0 +1,35 @@
+# Defining CIDR Block for VPC
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+# Defining CIDR Block for 1st subnet
+variable "subnet1_cidr" {
+  default = "10.0.1.0/24"
+}
+
+# Defining CIDR Block for 2nd subnet
+variable "subnet2_cidr" {
+  default = "10.0.2.0/24"
+}
+
+# Defining CIDR Block for 3rd subnet
+variable "subnet3_cidr" {
+  default = "10.0.3.0/24"
+}
+
+# Defining CIDR Block for 4th subnet
+variable "subnet4_cidr" {
+  default = "10.0.4.0/24"
+}
+
+# Defining CIDR Block for 5th subnet
+variable "subnet5_cidr" {
+  default = "10.0.5.0/24"
+}
+
+# Defining CIDR Block for 6th subnet
+variable "subnet6_cidr" {
+  default = "10.0.6.0/24"
+}
+
